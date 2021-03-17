1ac320093aac90dcb59b630568c7c1890c603d03
welkatz <73112987+welkatz@users.noreply.github.com>
2021-03-17 13:21:35 +0100

    first draft

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("huhu");
+const mixedMessages = () => {
+    const weather = ["gloomy", "sunny", "cloudy", "stormy", "crazy", "hazy", "foggy", "rainy", "overcast", "chilly"];
+    console.log(weather.length);
+    const suggestion = ["a swim", "a hike", "a picknick", "a ride", "a run", "a walk in the park", "some gardening", "visit to the zoo", "a movie", "a beer"];
+    console.log(suggestion.length);
+    //const messWeather = "";
+    //const messSuggestion = "";
+
+    //console.log("Today the weather is " + messWeather + ". You should go out for " + messSuggestion + ".");
+}
+
+mixedMessages();
+
