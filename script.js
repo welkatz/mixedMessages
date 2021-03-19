f0cdf3cc7c166f0ac15887136eb87c0bb5db14ea
welkatz <73112987+welkatz@users.noreply.github.com>
2021-03-19 09:24:35 +0100

    final version

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const mixedMessages = () => {
     const weather = ["gloomy", "sunny", "cloudy", "stormy", "crazy", "hazy", "foggy", "rainy", "overcast", "chilly"];
-    console.log(weather.length);
     const suggestion = ["a swim", "a hike", "a picknick", "a ride", "a run", "a walk in the park", "some gardening", "visit to the zoo", "a movie", "a beer"];
-    console.log(suggestion.length);
-    //const messWeather = "";
-    //const messSuggestion = "";
-
-    //console.log("Today the weather is " + messWeather + ". You should go out for " + messSuggestion + ".");
+    let messWeather = weather[Math.floor(Math.random() * 10)];
+    let messSuggestion = suggestion[Math.floor(Math.random() * 10)];
+    console.log("Today the weather is " + messWeather + ". You should go out for " + messSuggestion + ".");
 }
 
 mixedMessages();
